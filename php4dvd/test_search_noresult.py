9be0fe3b933c8d725bd2ef61d69bd42a58db668d
shurius <zviahina@gmail.com>
2015-10-06 00:36:42 +0300

    задания 3,4

--- php4dvd/test_search_noresult.py
+++ php4dvd/test_search_noresult.py
@@ -10,7 +10,7 @@ import unittest, time, re
 class Untitled7(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
+        self.driver.implicitly_wait(10)
         self.base_url = "http://localhost/php4dvd/"
         self.verificationErrors = []
         self.accept_next_alert = True
@@ -23,12 +23,24 @@ class Untitled7(unittest.TestCase):
         driver.find_element_by_name("password").clear()
         driver.find_element_by_name("password").send_keys("admin")
         driver.find_element_by_name("submit").click()
-        driver.find_element_by_id("q").click()
+        driver.find_element_by_css_selector("img[alt=\"Add movie\"]").click()
+        driver.find_element_by_name("name").click()
+        driver.find_element_by_name("name").clear()
+        driver.find_element_by_name("name").send_keys("movie_2_delete")
+        driver.find_element_by_name("year").click()
+        driver.find_element_by_name("year").clear()
+        driver.find_element_by_name("year").send_keys("1999")
+        driver.find_element_by_id("submit").click()
+        driver.find_elements_by_xpath("//*[contains(text(), 'movie_2_delete')]")
+        driver.find_element_by_css_selector("img[alt=\"Remove\"]").click()
+        self.assertRegexpMatches(self.close_alert_and_get_its_text(), r"^Are you sure you want to remove this[\s\S]$")
+        self.assertFalse(self.is_element_present(By.LINK_TEXT, "movie_2_delete"))
+        driver.find_element_by_css_selector("h1").click()
         driver.find_element_by_id("q").clear()
-        driver.find_element_by_id("q").send_keys("samplesample")
-        driver.find_element_by_id("q").send_keys(Keys.RETURN)
+        driver.find_element_by_id("q").send_keys("movie_2_delete")
+        driver.find_element_by_id("q").click()
+        driver.find_element_by_id("q").send_keys(Keys.RETURN)  
         driver.find_elements_by_xpath("//*[contains(text(), 'No movies where found.')]")
-    
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException, e: return False
